ef72bc75ceb2fdbfcc9f4ceab128a268b2f114aa
Andrew <kungpaogao@gmail.com>
2022-06-06 11:56:24 -0400

    Style navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useRoutes } from "solid-app-router";
-import { Component, lazy } from "solid-js";
+import { Component } from "solid-js";
 import Navigation from "./components/Navigation";
 import { routes } from "./routes";
 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,18 +1,43 @@
 import { Link } from "solid-app-router";
+import { FiMenu } from "solid-icons/fi";
+import { JSXElement } from "solid-js";
 
 const Navigation = () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-      </ul>
+      <input type="checkbox" class="peer hidden" id="menu-checkbox" />
+      <div class="flex">
+        <label class="p-3 md:hidden" for="menu-checkbox">
+          <FiMenu size={24} />
+        </label>
+      </div>
+      <div class="linear max-h-0 overflow-hidden transition-all duration-500 peer-checked:max-h-28 md:max-h-[none]">
+        <ul class="flex flex-col gap-x-3 p-3 md:flex-row">
+          <Link href="/" class="hidde hover:animate-pulse md:block">
+            高
+          </Link>
+          <span class="flex-1" />
+          <NavigationItem href="/" className="md:hidden">
+            Home
+          </NavigationItem>
+          <NavigationItem href="/projects">Projects</NavigationItem>
+          <NavigationItem href="/about">About</NavigationItem>
+        </ul>
+      </div>
     </nav>
   );
 };
+
+type NavigationItemProps = {
+  href: string;
+  children: JSXElement;
+  className?: string;
+};
+
+const NavigationItem = ({ href, children, className }: NavigationItemProps) => (
+  <li class={`text-gray-500 transition-colors hover:text-black ${className}`}>
+    <Link href={href}>{children}</Link>
+  </li>
+);
 
 export default Navigation;
