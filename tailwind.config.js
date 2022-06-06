f825db2723c1b4cce3ce8feb570373d2b1727615
Andrew <kungpaogao@gmail.com>
2022-06-06 11:51:56 -0400

    Add slide animations, full height to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-down": {
+          "0%": { opacity: 0, transform: "translateY(-10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        "slide-up": {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "slide-down": "slide-down 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+        "slide-up": "slide-up 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+      },
+    },
   },
   plugins: [],
 };
